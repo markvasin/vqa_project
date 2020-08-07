72fd8687253be445f6aa16ce741a7a63cee8e67e
Vasin Srisupavanich <mvsmark@gmail.com>
2020-08-07 18:21:55 +0100

    Init vl transformer

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ import vqa_project.models.baseline
 import vqa_project.models.mac_network
 import vqa_project.models.baseline_gqa
 import vqa_project.dataset.gqa_builder
+import vqa_project.models.vl_transformer
--- download.sh
+++ download.sh
@@ -1,7 +1,17 @@
+#mkdir data
+#cd data
+#wget https://nlp.stanford.edu/data/gqa/data1.2.zip
+#unzip data1.2.zip
+#wget http://nlp.stanford.edu/data/glove.6B.zip
+#unzip glove.6B.zip
+#cd ../
+
 mkdir data
 cd data
-wget https://nlp.stanford.edu/data/gqa/data1.2.zip
-unzip data1.2.zip
-wget http://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip
-cd ../
+wget https://nlp.stanford.edu/data/gqa/spatialFeatures.zip
+unzip spatialFeatures.zip
+
+python merge.py --path data --name spatial
+
+wget https://nlp.stanford.edu/data/gqa/questions1.2.zip
+unzip questions1.2.zip
